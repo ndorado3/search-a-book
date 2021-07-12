e115c6b9843225fc708ff5af4c3d39da62f7528f
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-07-12 14:05:57 -0400

    added the typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require("apollo-server-express");
+
+//to define typeDefs we put them in back teeks ` `
+//type class: is how I want the data to be represent. With the properities
+// & type of query
+// type Book the name is coming from the Model Class
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID1
+    user: User
+  }
+  input savedBook {
+    bookId: String
+    title: String
+    description: String
+    image: String
+    link: String
+    authors: [String]
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String, email: String!, password: String!): Auth
+    savedBook(input: savedBook): User
+    removeBook(bookID: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+// Import the ApolloServer class
+const { ApolloServer } = require("appollo-server-express");
+const path = require("path");
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require("./config/connection");
+
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+//TODO: figure out if I need this line of code below 
 app.use(routes);
 
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 API server running on port ${PORT}!`);
+    console.log(`🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
